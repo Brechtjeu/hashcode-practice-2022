01ff460a32a612032307b4fbaafc9ca677c99762
Brechtjeu <vanroybrecht@gmail.com>
2022-02-21 20:20:59 +0100

    fix import path

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from parse import parse_input
 
 if __name__ == '__main__':
 
-    persons = parse_input('inputs/a_an_example.in.txt')
+    persons = parse_input('../inputs/a_an_example.in.txt')
     print(persons)
