8bc7b5f772590b377227216ad4598892688837a2
Brechtjeu <vanroybrecht@gmail.com>
2022-02-21 19:58:49 +0100

    now creates the output folder

--- src/output.py
+++ src/output.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
 from zipfile import ZipFile
 
-
 def create_zip():
+    pathlib.Path('../outputs').mkdir(parents=True, exist_ok=True) 
     with ZipFile('../outputs/src.zip', 'w') as myzip:
         for file in os.listdir(os.curdir):
             myzip.write(file)
+
