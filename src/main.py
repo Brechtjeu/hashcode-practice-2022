7c6f35d00d6cb03518d6205408a1d8c21b8231b4
kaspervandenhaute <vanderkasper@gmail.com>
2022-02-21 20:44:35 +0100

    simple score based system

--- src/main.py
+++ src/main.py
@@ -4,3 +4,23 @@ if __name__ == '__main__':
 
     persons = parse_input('../inputs/a_an_example.in.txt')
     print(persons)
+
+    scores = dict()
+
+    for person in persons:
+
+        for like in person.likes:
+            if like in scores:
+                scores[like] += 1
+            else:
+                scores[like] = 1
+            
+        for like in person.likes:
+            if like in scores:
+                scores[like] -= 1
+            else:
+                scores[like] = -1
+
+    ingredients = [ingredient for ingredient, score in scores.items() if score > 0]
+    print(ingredients)
+
